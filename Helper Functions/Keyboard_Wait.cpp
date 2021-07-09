85c4cffd836183d667e535cf355b0a2dd181b87d
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-07-09 10:02:45 +0530

    Keyboard Wait

--- Helper Functions/Keyboard_Wait.cpp
+++ Helper Functions/Keyboard_Wait.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 	cout<<"Hello, For every 60 seconds a minute passes in Africa!"<<endl;
-	cout<<"Together we can stop this. So, please spred the word"<<endl<<endl;
+	cout<<"Together we can stop this. So, please spread the word"<<endl<<endl;
 	cout<<"Press any key to exit"<<endl;
 	
 	getchar();	//Main Focus
